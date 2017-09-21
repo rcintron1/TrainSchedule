551f5271b7cbc64980df0ffaf45b64df99440564
rcintron1 <rolando.cintron@gmail.com>
2017-09-20 23:52:42 -0400

    Added logic to read and write to DB. Now I have to add logic to delete selected row

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -14,29 +14,40 @@ var database = firebase.database();
 //add data to DB
 function writeUserData(trainName, destination, firstTrainTime, frequency) {
   console.log([trainName,destination,firstTrainTime,frequency]);
-  firebase.database().ref().set({
+  firebase.database().ref().push({
     trainName: trainName,
     destination: destination,
     firstTrainTime: firstTrainTime,
     frequency: frequency
   });
 }
+function appendTable(keys){
+  tdtn= $("<td>").append(keys["tn"]);
+  tddst= $("<td>").append(keys["ds"]);
+  tdftt = $("<td>").append(keys["ft"]);
+  tdfrq = $("<td>").append(keys["fq"]);
+  tr = $("<tr>").attr("key",keys["key"]);
+  tr.append(tdtn,tddst,tdftt,tdfrq);
+  $("#tbody").append(tr);
+}
 $(document).ready(function() {
   //add eventhandlers for fillOutForm buttons
-  var tname = $("#trainName").val();
-  var dest = $("#dest").val();
-  var fTTime = $("#fTrainTime").val();
-  var freq = $("#frequency").val();
   $("#btnSubmit").on("click", function() {
-
+    var tname = $("#trainName").val();
+    var dest = $("#dest").val();
+    var fTTime = $("#fTrainTime").val();
+    var freq = $("#frequency").val();
     writeUserData(tname,dest,fTTime,freq);
   });
 });
 
-database.ref().on("value", function(snapshot) {
-      console.log(snapshot.val());
-      $("#trainName").val(snapshot.val().trainName);
-      $("#dest").val(snapshot.val().destination);
-      $("#fTrainTime").val(snapshot.val().firstTrainTime);
-      $("#frequency").val(snapshot.val().frequency);
+database.ref().on("child_added", function(snapshot) {
+      console.log(snapshot);
+      var key = {};
+      key["tn"] = snapshot.val().trainName;
+      key["ds"] = snapshot.val().destination;
+      key["ft"] = snapshot.val().firstTrainTime;
+      key["fq"] = snapshot.val().frequency;
+      key["key"] = snapshot.key;
+      appendTable(key);
 });
